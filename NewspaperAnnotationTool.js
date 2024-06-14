7242d42e134c1b1006ffdd7195386b4608915d86
Mohit Dhanjani <md@iconappcom.in>
2024-06-15 01:01:35 +0530

    Source code of Newspaper Annotation Tool (NAT) V1.

---
+++ NewspaperAnnotationTool.js
@@ -0,0 +1,36 @@
+/*
+This file host some basic initial loading code for Newspaper Annotation Tool (NAT).
+Export related operations done during import of images (extracting date, volume, page, etc. information
+from the filename) and export (creating a CSV file that can directly be used for analysis) are
+in the main via.js file.
+
+This tool was made by modifying VGG VIA version 2 by Mohit Dhanjani (Senior Research Fellow at Manipal University Jaipur).
+ */
+/*
+Performs the following function when the VIA calls.
+Loads the Newspaper Annotation Tool (NAT) by adding Headline, Byline, Body region attributes.
+Also adds Date, Page, Volume, Issue and Publication file attributes.
+*/
+function _via_load_submodules() {
+    nat_add_new_attribute('Headline', VIA_ATTRIBUTE_TYPE.TEXT);
+    nat_add_new_attribute('Byline', VIA_ATTRIBUTE_TYPE.TEXT);
+    nat_add_new_attribute('Body', VIA_ATTRIBUTE_TYPE.TEXT);
+    _via_attributes['file']['Date'] = { 'type': 'text' };
+    _via_attributes['file']['Date'].default_value = '';
+    _via_attributes['file']['Page'] = { 'type': 'text' };
+    _via_attributes['file']['Page'].default_value = '';
+    _via_attributes['file']['Volume'] = { 'type': 'text' };
+    _via_attributes['file']['Volume'].default_value = '';
+    _via_attributes['file']['Issue'] = { 'type': 'text' };
+    _via_attributes['file']['Issue'].default_value = '';
+    _via_attributes['file']['Publication'] = { 'type': 'text' };
+    _via_attributes['file']['Publication'].default_value = '';
+    show_message('Successfully loaded News Annotation Tool.');
+}
+//Function to quickly add region attributes.
+function nat_add_new_attribute(attribute_id, type) {
+    _via_attributes[_via_attribute_being_updated][attribute_id] = {};
+    _via_attributes[_via_attribute_being_updated][attribute_id].type = type;
+    _via_attributes[_via_attribute_being_updated][attribute_id].description = '';
+    _via_attributes[_via_attribute_being_updated][attribute_id].default_value = '';
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+# Newspaper Annotation Tool (NAT)
+
+Newspaper Annotation Tool (NAT) is an open-source tool designed to assist media researchers and academicians in coding newspapers faster for content analysis. NAT is built by modifying the code of VGG Image Annotator version 2.
+
+## Features
+
+- **Automatic Attribute Extraction**: Extracts publication, date, page, volume, and issue information from the file name and automatically reflects that in the file attributes.
+- **Common Region Attributes**: Adds common region attributes like headline, byline, and body.
+- **CSV Export**: Coded data can be exported in CSV format for easy import into different data analysis software.
+
+## Installation
+
+To use NAT, download the release package and open the `index.html` file in your web browser.
+
+## Usage
+
+1. **Add Images**: Presently, only individual image files can be added.
+2. **Add Additional Attributes**: You can add additional attributes according to your needs, like the type of content (Ad, Article, Classified, etc.).
+3. **Annotate**: Use the annotation tool to mark regions and add attributes such as headline, byline, and body using the Annotation Editor.
+4. **Export**: Once the annotations are complete, export the data as a NAT CSV file for further analysis.
+
+
+## Future Roadmap
+
+Following are the features and enhancements that are planned for the future development:
+
+1. [ ] Add PDF support: Presently only individual images can be added.
+2. [ ] OCR support: An optional feature to be able to connect to text extraction services like AWS Textract.
+
+## License
+
+This project is licensed under the BSD 2-Clause "Simplified" License.
+
+## Acknowledgements
+
+NAT is built by modifying the code of VGG Image Annotator version 2.
+
+## Contact
+
+NAT was developed by Mohit Dhanjani, a Senior Research Fellow at the School of Media and Communication, Manipal University Jaipur. For any questions or suggestions, feel free to open an issue.
+
+---
